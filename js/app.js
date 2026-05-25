83579c6cbee34c3620dcf3374c30ab8aa96bb184
Luiz e Ruan <luizeeruan2026@gmail.com>
2026-05-25 15:29:14 -0300

    Finalizando projeto FutLourdão

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 import { jogadores, banners } from "./objetos.js";
 
+const meuGrid = document.querySelector('#grid');
+
+// recuperar o elemento HTML que receberá o grid
+// montando o grid - inserindo as images
+banners.forEach(banner => {
+    meuGrid.innerHTML += `
+    <img src="${banner}" alt="imagem do banner" class= "img-grid">
+    `
+});
+
 // montando o grid das imagens
+
+const meuHero = document.querySelector('#hero');
+
+// função para trocar o banner
+function trocarBanner(src) {
+    meuHero.style.backgroundImage = `var(--gradient), url(${src})`;
+
+
+    // aplicar a animação toda vez que a imagem mudar
+    meuHero.style.animation = 'none';
+    meuHero.offsetHeight;
+    meuHero.style.animation = 'myhero .5s linear forwards';
+}
+
+// definindo o click da imagem
+document.addEventListener('click', function (event) {
+    // verificar se o cara que recebeu o click possui a classe css img-grid
+    if (event.target.classList.contains('img-grid')) {
+        const src = event.target.getAttribute('src');
+        trocarBanner(src);
+
+    }
+
+})
+
+// deixar a imagem aleatória, nova imagem cada vez q a página é carregada
+const numeroSorteado = Math.floor(Math.random() * banners.length);
+const bannerinicial = banners[numeroSorteado]; ~
+    trocarBanner(bannerinicial)
