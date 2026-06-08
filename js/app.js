3a85db14b1d11e7f79575e7f34af6df0d8d2ee88
Luiz e Ruan <luizeeruan2026@gmail.com>
2026-06-08 13:34:28 -0300

    Gerando Cards

--- js/app.js
+++ js/app.js
@@ -40,3 +40,47 @@ document.addEventListener('click', function (event) {
 const numeroSorteado = Math.floor(Math.random() * banners.length);
 const bannerinicial = banners[numeroSorteado]; ~
     trocarBanner(bannerinicial)
+
+// Montando os cards
+
+// recuperando o elemento HTML que receberá os cards
+const elGrid = document.querySelector('#grid-cards');
+
+let totalJogadores = 0;
+
+// percorrer o Array e para cada um dos jogadores montar um card
+jogadores.forEach(jogador => {
+
+    // verificamdo se o jogador é brasileiro
+    if (jogador.nacionalidade.toLowerCase() === 'brasil' ||
+        jogador.nacionalidade.toLowerCase() === 'espanha' ||
+        jogador.nacionalidade.toLowerCase() === 'argentina' ||
+        jogador.nacionalidade.toLowerCase() === 'geórgia') {
+
+
+        elGrid.innerHTML += `
+        <div class="col my-3">
+        <div class="card">
+        <img src="${jogador.foto}" class="card-img-top img-fluid jogador" alt="${jogador.nome}">
+        <div class="card-body">
+        <h5 class="card-title text-center">${jogador.nome}</h5>
+        <div class="card-bio">
+        <p>${jogador.clube_atual}</p>
+        <p>${jogador.posicao}</p>
+        <p>${jogador.idade} Anos</p>
+        <p>${jogador.nacionalidade}</p>
+        <p>${jogador.altura} cm </p>
+        <p>${jogador.peso} Kg</p>
+        </div>
+        </div>
+        </div>
+        </div>
+    `;
+    totalJogadores++;
+    console.log(totalJogadores)
+    }
+});
+
+// Exibindo o total de jogadores encontrados 
+
+document.querySelector('#totJogador').textContent = totalJogadores
